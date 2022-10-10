c6e6e16196ed4be17cbd3607a9b86ea7eb283c6e
kenleycl07 <78169956+kenleycl07@users.noreply.github.com>
2022-10-10 04:42:06 -0700

    Add files via upload

--- using lists/usl3.py
+++ using lists/usl3.py
@@ -1,5 +1,5 @@
 #################
-### using lists
+### using dictionaries
 ### part3
 #################
 
@@ -58,72 +58,3 @@ print(squares)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 cubes = [number**3 for number in numbers]
 print(cubes)
-
-# create list of repeated objects
-
-# create a list of 12 repeated zeros
-n = 12
-zeros = [0]*n
-print('list of zeros:', zeros)
-
-# create a list of 12 ones
-n = 12
-ones = [1]*n
-print('list of ones:', ones)
-
-# create a nested list using list comprehension
-repeated = [[n for n in range(6)] for m in range(2)]
-print(repeated)
-
-# create a nested list using list comprehension
-# including operations
-repeated = [[n + 1 for n in range(3)] for m in range(4)]
-print(repeated)
-
-repeated = [[n + m for n in range(3)] for m in range(4)]
-print(repeated)
-
-repeated = [[n - m for n in range(3)] for m in range(4)]
-print(repeated)
-
-# sums, average, and lists
-
-# compute the sum of a sequence of numbers and do the average
-total = 0.0
-count = 0
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-for number in numbers:
-    total += number
-    count += 1
-
-average = total / count
-print(total)
-print(average)
-
-# Write a Python program to sum all the items in a list.
-# Hints: Create a function sum_list() that takes one
-# parameter called 'items'. Your function Should return
-# the sum of all the numbers in the list. 
-def sum_list(items):
-    total = 0
-    for item in items:
-        total += item
-    return total
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(sum_list(numbers))
-
-"""conditions and lists"""
-
-# this program checks if there is any value in a list that
-# is greater than five. If a number is greater than five, the 
-# program breaks.
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-greater = False
-for number in numbers:
-    if number > 5:
-        greater = True
-        print('the program breaks at', number)
-        break
-
